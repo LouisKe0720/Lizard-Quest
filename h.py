2657d7d458903ff608b9935ccfd83a43131cdd9f
Louis0720 <Louiske720@gmail.com>
2025-01-12 14:07:24 -0500

    code condensing and appeal

--- h.py
+++ h.py
@@ -1,10 +1,9 @@
 import pygame
 from pygame.locals import *
 from pygame.sprite import *
-from pygame.time import *
 import random
+import time
 import mechanics
-
 
 # Initialize pygame
 pygame.init()
@@ -16,30 +15,34 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 grass = (34, 139, 34)
 
-# Screen Settings
+# Screen settings
 screen_width, screen_height = 600, 480
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_icon(pygame.image.load('LQ LOGO.png'))
-pygame.display.set_caption("Lizard Quest")
+pygame.display.set_caption("Test Walking")
 screen.fill(white)
 
-
-# Clock and Font
-font = pygame.font.SysFont('Arial', 30)
+# Clock and font
 clock = pygame.time.Clock()
+font = pygame.font.SysFont('Arial', 30)
 
-x = 300
-y = 220
-dx = 500
-dy = 220
-speed = 5
+# Load images
+p_block = pygame.image.load("PURPLE BLOCK TEXTURE.png")
+p_block = pygame.transform.scale(p_block, (22, 22))
+p_region = pygame.image.load("PURPLE REGION TEXTURE.png")
+p_water = pygame.image.load("WATER TEXTURE.png")
+
+# Load music
+pygame.mixer.music.load("DQ Overture XI.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 
 # Define buttons
 start_button = pygame.Rect(20, 170, 400, 75)
 time_button = pygame.Rect(20, 370, 400, 75)
 back_button = pygame.Rect(300, 370, 100, 75)
 skill_button = pygame.Rect(5, 285, 110, 40)
-items_button = pygame.Rect(5, 330, 110, 40)
+items_button = pygame.Rect(5, 315, 110, 40)
 flee_button = pygame.Rect(5, 373, 110, 40)
 
 # Load battle screen images
@@ -53,12 +56,7 @@ fleePotion_image = pygame.image.load('ITEM DESCRIPTIONS - FLEE POTION.png')
 healOrb_image = pygame.image.load('ITEM DESCRIPTIONS - HEAL ORB.png')
 magicUpPotion_image = pygame.image.load('ITEM DESCRIPTIONS - MAGIC UP!.png')
 
-#MUSIC START
-pygame.mixer.music.load("DQ Overture XI.mp3")
-pygame.mixer.music.set_volume(0.5) 
-pygame.mixer.music.play(-1)
-
-
+# Define classes
 class FallingPixel:
     def __init__(self):
         self.x = random.randint(0, screen_width)
@@ -71,47 +69,6 @@ class FallingPixel:
     def draw(self, surface):
         pygame.draw.rect(surface, black, (self.x, self.y, 20, 20))
 
-def screen_cover(surface, cover_height):
-    pygame.draw.rect(surface, (150, 150, 150), (0, -5, screen_width, cover_height + 55))
-    pygame.draw.rect(surface, (100, 100, 100), (0, -10, screen_width, cover_height + 50))
-    pygame.draw.rect(surface, (50, 50, 50), (0, -20, screen_width, cover_height + 40))
-    pygame.draw.rect(surface, black, (0, 0, screen_width, cover_height))
-
-p_block = pygame.image.load("PURPLE BLOCK TEXTURE.png")
-p_block = pygame.transform.scale(p_block, (22, 22))
-p_region = pygame.image.load("PURPLE REGION TEXTURE.png")
-p_water = pygame.image.load("WATER TEXTURE.png")
-
-def draw_grass(surface, pixel_spacing):
-    for i in range(0, screen_width, pixel_spacing):
-        for j in range(0, screen_height, pixel_spacing):
-            surface.blit(p_region, (i, j))
-
-def draw_block(surface, x, y, final_x, final_y, pixel_spacing=20):
-    for i in range(x, final_x, pixel_spacing):
-        for j in range(y, final_y, pixel_spacing):
-            surface.blit(p_block, (i, j))
-
-pixel_spacing = 30
-
-def background(surface):
-    pygame.draw.rect(surface, (105, 201, 97), (0, 0, screen_width, screen_height))
-    draw_grass(screen, 15)
-
-    block_positions = [
-        (540, 300, 540 + 90, screen_height), (450, 360, 450 + 90, screen_height), (390, 420, 390 + 90, screen_height), (0, 0, 90, 120),
-        (0, 0, 45, 200), (0, 0, 20, 300)
-    ]
-
-    for x, y, final_x, final_y in block_positions:
-        draw_block(screen, x, y, final_x, final_y)
-
-num_pixels = 37
-falling_pixels = [FallingPixel() for i in range(num_pixels)]
-
-cover_height = 0
-pixel_falling = True
-
 class ImageSprite(Sprite):
     def __init__(self, filename):
         Sprite.__init__(self)
@@ -123,22 +80,32 @@ class ImageSprite(Sprite):
     def update(self, x, y):
         self.rect.center = (x, y)
 
-mutants = [ImageSprite('MUTANT 1.png'), ImageSprite('MUTANT 2.png'), ImageSprite('MUTANT 3.png'), ImageSprite('MUTANT 4.png'),
-           ImageSprite('MUTANT 5.png'), ImageSprite('MUTANT 6.png'), ImageSprite('MUTANT 7.png')]
+# Define functions
+def draw_grass(surface, pixel_spacing):
+    for i in range(0, screen_width, pixel_spacing):
+        for j in range(0, screen_height, pixel_spacing):
+            surface.blit(p_region, (i, j))
 
-mutant = random.choice(mutants)
-frame_1 = ImageSprite('PIXEL TEST F1.png')
-frame_2 = ImageSprite('PIXEL TEST F2.png')
-frame_3 = ImageSprite('PIXEL TEST F3.png')
+def draw_block(surface, x, y, final_x, final_y, pixel_spacing=20):
+    for i in range(x, final_x, pixel_spacing):
+        for j in range(y, final_y, pixel_spacing):
+            surface.blit(p_block, (i, j))
 
-frames = [frame_1, frame_2, frame_3]
+def background(surface):
+    pygame.draw.rect(surface, (105, 201, 97), (0, 0, screen_width, screen_height))
+    draw_grass(screen, 15)
+    block_positions = [
+        (540, 300, 540 + 90, screen_height), (450, 360, 450 + 90, screen_height), (390, 420, 390 + 90, screen_height), (0, 0, 90, 120),
+        (0, 0, 45, 200), (0, 0, 20, 300)
+    ]
+    for x, y, final_x, final_y in block_positions:
+        draw_block(screen, x, y, final_x, final_y)
 
-frame_num = 0
-frame_count = 0
-frame_change = 5
-
-sprite = frame_1
-sprite_group = Group(sprite, mutant)
+def screen_cover(surface, cover_height):
+    pygame.draw.rect(surface, (150, 150, 150), (0, -5, screen_width, cover_height + 55))
+    pygame.draw.rect(surface, (100, 100, 100), (0, -10, screen_width, cover_height + 50))
+    pygame.draw.rect(surface, (50, 50, 50), (0, -20, screen_width, cover_height + 40))
+    pygame.draw.rect(surface, black, (0, 0, screen_width, cover_height))
 
 def show_title_screen():
     title_image = pygame.image.load('LQ TITLE SCREEN.png')
@@ -192,6 +159,7 @@ def show_time_screen():
         pygame.draw.rect(screen, black, back_button)
         pygame.display.flip()
         clock.tick(60)
+
 def show_battle_screen():
     defenseUpPotion, fleePotion, healOrb, magicUpPotion = mechanics.item_appear()
     waiting = True
@@ -207,18 +175,19 @@ def show_battle_screen():
                     show_items_screen(defenseUpPotion, fleePotion, healOrb, magicUpPotion)
                 elif flee_button.collidepoint(event.pos):
                     show_flee_screen()
-        draw_buttons()
         screen.blit(battle_screen, (0, 0))
+        draw_buttons()
         pygame.display.update()
+
 def draw_buttons():
     pygame.draw.rect(screen, white, skill_button)
     pygame.draw.rect(screen, white, items_button)
     pygame.draw.rect(screen, white, flee_button)
 
-def show_flee_screen():
-    flee_opened = True
-    while flee_opened:
-        screen.blit(flee_image, (0, 0))
+def show_skills_screen():
+    skills_opened = True
+    while skills_opened:
+        screen.blit(skills_image, (0, 0))
         pygame.display.flip()
         clock.tick(60)
         for event in pygame.event.get():
@@ -226,8 +195,9 @@ def show_flee_screen():
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if flee_button.collidepoint(event.pos) or skill_button.collidepoint(event.pos) or items_button.collidepoint(event.pos):
-                    flee_opened = False
+                if skill_button.collidepoint(event.pos) or items_button.collidepoint(event.pos) or flee_button.collidepoint(event.pos):
+                    skills_opened = False
+
 def show_items_screen(defenseUpPotion, fleePotion, healOrb, magicUpPotion):
     items_opened = True
     while items_opened:
@@ -250,10 +220,10 @@ def show_items_screen(defenseUpPotion, fleePotion, healOrb, magicUpPotion):
                 if items_button.collidepoint(event.pos) or skill_button.collidepoint(event.pos) or flee_button.collidepoint(event.pos):
                     items_opened = False
 
-def show_skills_screen():
-    skills_opened = True
-    while skills_opened:
-        screen.blit(skills_image, (0, 0))
+def show_flee_screen():
+    flee_opened = True
+    while flee_opened:
+        screen.blit(flee_image, (0, 0))
         pygame.display.flip()
         clock.tick(60)
         for event in pygame.event.get():
@@ -261,20 +231,36 @@ def show_skills_screen():
                 pygame.quit()
                 exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if skill_button.collidepoint(event.pos) or items_button.collidepoint(event.pos) or flee_button.collidepoint(event.pos):
-                    skills_opened = False
+                if flee_button.collidepoint(event.pos) or skill_button.collidepoint(event.pos) or items_button.collidepoint(event.pos):
+                    flee_opened = False
 
-mechanics.start_stopwatch()  
+# Main game loop
+mechanics.start_stopwatch()
 show_title_screen()
 show_start_screen()
 pygame.mixer.music.load("DQ Adventure Theme.mp3")
 pygame.mixer.music.play(-1)
 
+x, y = 300, 220
+dx, dy = 500, 220
+speed = 5
 direction = None
 moving = False
 running = True
-start = False
 battle_screen_shown = False
+pixel_falling = True
+cover_height = 0
+num_pixels = 37
+falling_pixels = [FallingPixel() for _ in range(num_pixels)]
+
+mutants = [ImageSprite(f'MUTANT {i}.png') for i in range(1, 8)]
+mutant = random.choice(mutants)
+frames = [ImageSprite(f'PIXEL TEST F{i}.png') for i in range(1, 4)]
+frame_num = 0
+frame_count = 0
+frame_change = 5
+sprite = frames[0]
+sprite_group = Group(sprite, mutant)
 
 while running:
     for event in pygame.event.get():
@@ -309,13 +295,8 @@ while running:
             frame_count = 0
             frame_num = (frame_num + 1) % len(frames)
             sprite.image = frames[frame_num].image
-            if direction == 'left':
-                if frame_num != 0:
-                    sprite.image = pygame.transform.flip(sprite.image, True, False)
-                else:
-                    sprite.image = frames[frame_num].image
-            else:
-                sprite.image = frames[frame_num].image
+            if direction == 'left' and frame_num != 0:
+                sprite.image = pygame.transform.flip(sprite.image, True, False)
 
     sprite.update(x, y)
     mutant.update(dx, dy)
@@ -341,9 +322,9 @@ while running:
 
         screen_cover(screen, cover_height)
     
-    while pixel_falling == False and battle_screen_shown == True:
+    if not pixel_falling and battle_screen_shown:
         show_battle_screen()
-            
+
     pygame.display.flip()
     clock.tick(60)
 
